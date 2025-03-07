6ba4cffe2ceb92ceb97cd824191eff817bd370d5
Andrea Salvioni <andrea.salvioni@itsincom.it>
2025-03-07 09:52:18 +0100

    update

--- UniversityWebApp/DATA/Exam.cs
+++ UniversityWebApp/DATA/Exam.cs
@@ -3,14 +3,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace UniversityWebApp.DATA
 {
-    [PrimaryKey("StudentId", "CourseTipeId")]
     public class Exam
     {
         public int StudentId { get; set; }
         public int CourseTipeId { get; set; }
-        public int Grade { get; set; }
-        [ForeignKey("StudentId")]
-        public Student? Student { get; set; }
+        public required string Location { get; set; }
+        public required DateTime Date { get; set; }
+
         [ForeignKey("CourseTipeId")]
         public CourseTipe? CourseTipe { get; set; }
     }
