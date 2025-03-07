c4474dfeeff37e565408bc5b7883d52ce7a303f5
Andrea Salvioni <andrea.salvioni@itsincom.it>
2025-03-07 10:01:39 +0100

    pippo

---
+++ UniversityWebApp/DTO/ExamResultDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace UniversityWebApp.DTO
+{
+    public class ExamResultDTO
+    {
+        public int StudentId { get; set; }
+        public int ExamId { get; set; }
+        public int Grade { get; set; }
+        public enum Result { Passed, Failed, Rejected }
+        public StudentDTO? StudentDTO { get; set; }
+        public ExamDTO? ExamDTO { get; set; }
+    }
+}
